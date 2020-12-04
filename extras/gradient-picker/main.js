82b7d85801a1728413d18969328076b244b641ec
Hallef Santos <hallefsantos@gmail.com>
2020-12-04 08:44:14 -0300

    Extra - Gradient Picker

---
+++ extras/gradient-picker/main.js
@@ -0,0 +1,20 @@
+const pickers = document.querySelectorAll('.picker input')
+const gradient = {};
+
+pickers.forEach((picker, index) => {
+   picker.addEventListener('input', (e) => {
+      setColor(e.target, index)
+   })
+   setColor(picker, index)
+})
+
+function setColor(picker, index) {
+   const pickerColor = picker.previousSibling.previousSibling
+   pickerColor.style.backgroundColor = picker.value
+   const pickerLabel = picker.nextSibling.nextSibling
+   pickerLabel.innerText = picker.value
+   gradient[index] = picker.value
+   document.documentElement.style.setProperty(`--gradient-${index}`, picker.value);
+}
+
+console.log({gradient})
