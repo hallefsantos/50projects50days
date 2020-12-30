2d8acfac7895f43399856f6aff65ffe979ddfeda
Hallef Santos <hallefsantos@gmail.com>
2020-12-30 11:42:15 -0300

    Day 10 complete

---
+++ day10-dad-jokes/main.js
@@ -0,0 +1,18 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+generateJoke()
+
+jokeBtn.addEventListener('click', generateJoke)
+
+async function generateJoke() {
+   const config = {
+      headers: {
+         'Accept': 'application/json'
+      }
+   }
+
+   const res = await fetch('https://icanhazdadjoke.com/', config)
+   const data = await res.json()
+   jokeEl.innerText = data.joke
+}
